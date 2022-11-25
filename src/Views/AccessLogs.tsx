c4d113eefbdb7d0230933830f0b93416b5ced932
bmoraleTSU <bmorale1@my.tnstate.edu>
2022-11-25 15:26:50 -0600

    Finished makeshift accces logs page.

--- src/Views/AccessLogs.tsx
+++ src/Views/AccessLogs.tsx
@@ -1,12 +1,85 @@
 import "../App.css";
+import { useState } from "react";
 
 
 function AccessLogs() {
-    return (
-      <div>
+  // Handles burger click
+  function onClickHandler(){
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+    });
+  }
+
+  // Will be used to change icons of Return Home button on mouse hover
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+      setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+      setIsHovering(false);
+  };
+
+  return (
+    <>
+      <div className="hero-head">
+            <section className="section">
+                <nav className="navbar has-shadow is-fixed-top test-app-header" role="navigation" aria-label="main navigation">
+                    <div className="navbar-brand">
+                        <div className="navbar-item">
+                        <img
+                            src={require("../pageTitles/accesslogs_title.png")}
+                            alt="Logo"
+                            width="auto"
+                            height="auto"
+                        />
+                        </div>
+                        {/* <div className="navbar-item">
+                            <h1 className="title is-3 is-family-secondary has-text-weight-bold is-italic">Client List</h1>
+                        </div> */}
+                        <button className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={onClickHandler}>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </button>
+                    </div>
+                    <div id="navbarBasicExample" className="navbar-menu">
+                        <div className="navbar-end">
+                            <div className="navbar-item">
+                                <div className="field is-grouped">
+                                    <p className="control">
+                                        <a className="button is-success is-light" href="/" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+                                            <span className="icon">
+                                                <i className={isHovering ? 'fas fa-duotone fa-house-user fa-bounce' : 'fas fa-duotone fa-house-user'}></i>
+                                            </span>
+                                            <span>
+                                                Return Home
+                                            </span>
+                                        </a>
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </section>
+      </div>
+      <div className="hero-body">
         <h2>This is where the access logs would go!</h2>
       </div>
-    );
+    </>
+  );
 }
 
 export default AccessLogs;
